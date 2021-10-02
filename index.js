97b57a25e1cf4f591315f693965ae539b609af86
Zedan Saheer <zed.saheer5@gmail.com>
2021-10-03 00:02:57 +0530

    setting up : REST almost complete

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories")
 
 dotenv.config();
 app.use(express.json())
@@ -14,7 +16,10 @@ mongoose.connect(process.env.MONGO_URL,{
 }).then(console.log("Connected to mongoDB")).catch((err)=>console.log(err));
 
 app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute)
+app.use("/api/users",userRoute);
+app.use("/api/posts",postRoute);
+app.use("/api/category",categoryRoute)
+
 
 app.listen("5000",()=>{
     console.log("backend is running");
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const Post = require("../models/Post");
 
 //Update user
 router.put("/:id", async (req,res)=>{
@@ -12,12 +13,45 @@ router.put("/:id", async (req,res)=>{
     try {
         const updateUser = await User.findByIdAndUpdate(req.params.id,{
             $set:req.body,
-        });
-        res.status(200).json("Updated user")
+        },{new:true});
+        res.status(200).json(updateUser)
     } catch (error) {
         res.status(500).json(error);
     }}else{
         res.status(401).json("You can update only your account!")
+    }
+});
+
+
+//Delete 
+router.delete("/:id", async (req,res)=>{
+    if(req.body.userId===req.params.id){
+    try {
+        const user = await User.findById(req.params.id);
+        try {
+            await Post.deleteMany({username:user.username});
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("user has been deleted!")
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } catch (error) {
+        if(user){
+            res.status(404).json(error);
+        }
+    }}else{
+        res.status(401).json("You can delete only your account!")
+    }
+});
+
+//get user
+router.get("/:id", async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {password,...others}= user._doc;
+        res.status(200).json(others)
+    }catch(error){
+        res.status(500).json(error)
     }
 })
 
